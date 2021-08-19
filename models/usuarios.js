b43f9935dcdbfa5f139b23f4cff40763b1764851
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2021-08-19 11:14:43 -0500

    Defiendo Primeras Queries

---
+++ models/usuarios.js
@@ -0,0 +1,21 @@
+const usuariosQueries = {
+  insertUsuario: `
+    INSERT INTO
+      usuarios(
+        nombre,
+        email,
+        password,
+        status
+      )
+    VALUES
+      (?, ?, ?, ?)
+  `,
+  selectUsuarios: `
+    SELECT
+      *
+    FROM
+      usuarios
+    WHERE
+      status = 1
+  `,
+};
