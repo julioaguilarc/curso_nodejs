672494bb9d1a7a9af5c53136e3603984857f37da
Julio Aguilar Carmona <julio.aguilarc@gmail.com>
2021-08-20 10:21:26 -0500

    Ajuste en la variable de usuario de la BD

--- db/conexion.js
+++ db/conexion.js
@@ -2,11 +2,13 @@ const mariadb = require("mariadb");
 
 const config = {
   host: process.env.HOST,
-  user: process.env.USER,
+  user: process.env.DBUSER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   connectionLimit: process.env.CONN_LIMIT,
 };
+
+console.log(config);
 
 const pool = mariadb.createPool(config);
 
